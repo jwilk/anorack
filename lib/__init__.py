1947d222830803093a37f7ca90ff889908692fcb
Jakub Wilk <jwilk@jwilk.net>
2018-06-13 12:03:22 +0200

    lib: add explicit check against Python 3.0/3.1.

--- lib/__init__.py
+++ lib/__init__.py
@@ -2,4 +2,12 @@
 anorack's private modules
 '''
 
+import sys
+
 type(...)  # Python >= 3 is required
+if sys.version_info < (3, 2):
+    raise RuntimeError('Python >= 3.2 is required')
+
+__all__ = []
+
+# vim:ts=4 sts=4 sw=4 et
