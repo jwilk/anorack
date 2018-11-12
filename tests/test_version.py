fe91e63a4392bb374b4d9012c43e18bb36f22eb1
Jakub Wilk <jwilk@jwilk.net>
2018-11-12 21:49:42 +0100

    tests/test_version: check manpage version.

--- tests/test_version.py
+++ tests/test_version.py
@@ -36,4 +36,14 @@ def test_changelog():
     changelog_version = line.split()[1].strip('()')
     assert_equal(changelog_version, __version__)
 
+def test_manpage():
+    path = os.path.join(docdir, 'manpage.rst')
+    manpage_version = None
+    with open(path, 'rt', encoding='UTF-8') as file:
+        for line in file:
+            if line.startswith(':version:'):
+                manpage_version = line.split()[-1]
+                break
+    assert_equal(manpage_version, __version__)
+
 # vim:ts=4 sts=4 sw=4 et
