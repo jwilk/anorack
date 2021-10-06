c4ce55c85a63287edbb4a58209fa0be68e920030
Jakub Wilk <jwilk@jwilk.net>
2021-10-06 19:57:43 +0200

    tests/test_vcmp: add.

---
+++ tests/test_vcmp.py
@@ -0,0 +1,45 @@
+# Copyright © 2021 Jakub Wilk <jwilk@jwilk.net>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the “Software”), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from nose.tools import (
+    assert_equal,
+)
+
+import lib.espeak as M
+
+def cmp(x, y):
+    return (x > y) - (x < y)
+
+def test():
+    versions = '''
+    1
+    1.04.71
+    1.47.08
+    1.48.1
+    1.48.11
+    1.48.15
+    3
+    3.14
+    '''.split()
+    for i1, v1 in enumerate(versions):
+        for i2, v2 in enumerate(versions):
+            assert_equal(M.vcmp(v1, v2), cmp(i1, i2))
+
+# vim:ts=4 sts=4 sw=4 et
