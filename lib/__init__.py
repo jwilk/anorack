6866312b81b3ae173a90f74554167d7e06541f1a
Jakub Wilk <jwilk@jwilk.net>
2022-12-14 18:05:54 +0100

    Require Python 3.7.

--- lib/__init__.py
+++ lib/__init__.py
@@ -2,6 +2,8 @@
 anorack's private modules
 '''
 
-int(0_0)  # Python >= 3.6 is required
+# pylint: disable=multiple-statements
+async def _(): return f'{await "# Python >= 3.7 is required #"}'
+del _
 
 # vim:ts=4 sts=4 sw=4 et
